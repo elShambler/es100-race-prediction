bacfe137f66c2848d970efcf016970f6deee84ea
mas <mas@thorium>
2025-11-14 09:24:02 -0500

    feat: added timing errors and visualization to the dataset

--- src/eastern_states_pace_predict/pipelines/data_processing/pipeline.py
+++ src/eastern_states_pace_predict/pipelines/data_processing/pipeline.py
@@ -1,5 +1,9 @@
 from kedro.pipeline import Pipeline, node, pipeline
-from .nodes import preprocess_20162017_data
+from .nodes import (
+    preprocess_20162017_data,
+    flag_negative_elapsed_times,
+    visualize_elapsed_times_by_runner,
+)
 
 
 def create_pipeline(**kwargs) -> Pipeline:
@@ -10,6 +14,18 @@ def create_pipeline(**kwargs) -> Pipeline:
                 inputs="es_splits_20167",
                 outputs="es_processed_20162017",
                 name="preprocess_ultralive_node",
+            ),
+            node(
+                func=flag_negative_elapsed_times,
+                inputs="es_splits_20167",
+                outputs="es_splits_20167_filtered",
+                name="flag_timing_errors_node",
+            ),
+            node(
+                func=visualize_elapsed_times_by_runner,
+                inputs="es_splits_20167_filtered",
+                outputs="es_timing_validation_plot",
+                name="visualize_timing_errors_node",
             ),
         ]
     )
