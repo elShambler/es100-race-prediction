94f1e89d75234909ce5766d25ba6085202dc1981
mas <mas@thorium>
2026-07-10 10:57:46 -0400

    feat: added preview class for kedro to visualize Polars dataset as table

---
+++ src/eastern_states_pace_predict/datasets/polars_preview_csv_dataset.py
@@ -0,0 +1,14 @@
+from kedro_datasets._typing import TablePreview
+from kedro_datasets.polars import CSVDataset
+
+
+class PolarsPreviewCSVDataset(CSVDataset):
+    """polars.CSVDataset with a preview() method for Kedro Viz table display."""
+
+    def preview(self, nrows: int = 5) -> TablePreview:
+        data = self.load().head(nrows)
+        return {
+            "columns": data.columns,
+            "index": list(range(len(data))),
+            "data": [list(row) for row in data.rows()],
+        }
